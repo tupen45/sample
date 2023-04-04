ec64f7eff394d296e9c4f0ff2b088c0013c36378
Tbhcr <tupenshl@gmail.com>
2023-04-04 22:39:40 +0530

    file upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://tupen45.github.io/iloveyou/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          deploy react on github pages 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
